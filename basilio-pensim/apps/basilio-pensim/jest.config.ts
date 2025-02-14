c1312593465a58b17b86edd5a29951d95afe328b
Felipe Basilio <fcb.97@hotmail.com>
2025-02-14 03:14:03 -0300

    [MAJOR] Created MFEs and global library

---
+++ basilio-pensim/apps/basilio-pensim/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'basilio-pensim',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/basilio-pensim',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
---
+++ basilio-pensim/libs/global/src/lib/global/global.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-global',
+  imports: [CommonModule],
+  templateUrl: './global.component.html',
+  styleUrl: './global.component.css',
+})
+export class GlobalComponent {}
